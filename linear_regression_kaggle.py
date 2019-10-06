a18a1f7101fa85c57e1a334018e742db33226ab4
Mark Anderson <andersm3@tcd.ie>
2019-10-06 21:59:05 +0100

    ridge regression and write to csv

--- linear_regression_kaggle.py
+++ linear_regression_kaggle.py
@@ -1,17 +1,19 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as seabornInstance
+import seaborn as seab
 
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn import metrics
 
-dataset = pd.read_csv('data/tcd ml 2019-20 income prediction training (with labels).csv')
+dataset_training = pd.read_csv('data/tcd ml 2019-20 income prediction training (with labels).csv')
+dataset_predict = pd.read_csv('data/tcd ml 2019-20 income prediction test (without labels).csv')
+
 
 numerical_features = ['Year of Record','Age','Size of City','Wears Glasses','Body Height [cm]']
 categorical_features = ['Gender','Country','Profession','University Degree','Hair Color']
@@ -33,13 +35,19 @@ preprocessor = ColumnTransformer(
 
 lr = Pipeline(steps=[
     ('Preprocessor', preprocessor),
-    ('Linear Regression', LinearRegression())],verbose=True)
+    ('Ridge Regression', Ridge(alpha=0.5,fit_intercept=True,solver='auto'))],verbose=True)
 
-x_data = dataset.drop(['Instance','Income in EUR'], axis=1)
-y_data = dataset['Income in EUR']
+x_data = dataset_training.drop(['Instance','Income in EUR'], axis=1)
+y_data = dataset_training['Income in EUR']
 
 x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.3, random_state=0)
 
-lr.fit(x_train, y_train)
+x_predict = dataset_predict.drop(['Instance', 'Income'], axis=1)
 
-print(lr.score)
+model = lr.fit(x_train, y_train)
+predictions = pd.DataFrame(data=lr.predict(x_predict))
+
+predictions.to_csv('data/pred.csv')
+
+print(model.score(x_train, y_train))
+print(model.score(x_test, y_test))
